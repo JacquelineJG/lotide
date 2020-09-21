e6538f6e3efb5e0ba8144189a280ff3d1edf2658
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-09-21 18:16:34 +0000

    refactored testing for mocha and chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -4,3 +4,4 @@ const assertArraysEqual = require('../assertArraysEqual');
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [23, 2, 3]);
 assertArraysEqual([4, 2], [4, 2]);
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,11 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns Lighthouse Labs is Lighthouse Labs", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns two three", () => {
+    assert.deepEqual(tail(['one', 'two', 'three']), ['two', 'three']); 
+  });
+});
